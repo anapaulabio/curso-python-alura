db14a7ec36c4749630bac178c809a3c69a14cec7
Ana Paula <anapaulaoliveira.bio@outlook.com>
2023-01-31 13:09:17 -0300

    include rodadas

--- adivinhacao.py
+++ adivinhacao.py
@@ -5,8 +5,10 @@ print("*********************************")
 numero_secreto = 42
 
 total_de_tentativas = 3
+rodada = 1
 
-while (total_de_tentativas > 0):
+while (rodada <= total_de_tentativas):
+    print("Tentativa", rodada, "de", total_de_tentativas)
     chute_str = input("Digite o seu número: ")
     print("Você digitou: ", chute_str)
     chute = int(chute_str)
@@ -22,5 +24,7 @@ while (total_de_tentativas > 0):
             print("Você errou! O seu chute foi maior que o número secreto.")
         elif (menor):
             print("Você errou! O seu chute foi menor que o número secreto.")
+
+rodada = rodada + 1
 
 print("Fim do jogo")
