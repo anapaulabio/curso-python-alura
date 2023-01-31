c3341e002f3fb64173af214511e36aef2246000e
Ana Paula <anapaulaoliveira.bio@outlook.com>
2023-01-31 13:06:36 -0300

    include total de tentativas

--- adivinhacao.py
+++ adivinhacao.py
@@ -4,21 +4,23 @@ print("*********************************")
 
 numero_secreto = 42
 
-chute_q = input("Digite o seu número: ")
-print("Você digitou: ", chute_q)
+total_de_tentativas = 3
 
-chute = int(chute_q)
+while (total_de_tentativas > 0):
+    chute_str = input("Digite o seu número: ")
+    print("Você digitou: ", chute_str)
+    chute = int(chute_str)
 
-acertou = chute == numero_secreto
-maior = chute > numero_secreto
-menor = chute < numero_secreto
+    acertou = numero_secreto == chute
+    maior = chute > numero_secreto
+    menor = chute < numero_secreto
 
-if (acertou):
-    print("Você acertou!")
-else:
-    if (maior):
-        print("Você errou! O seu chute foi maior que o número secreto.")
-    elif (menor):
-        print("Você errou! O seu chute foi menor que o número secreto.")
+    if (acertou):
+        print("Você acertou!")
+    else:
+        if (maior):
+            print("Você errou! O seu chute foi maior que o número secreto.")
+        elif (menor):
+            print("Você errou! O seu chute foi menor que o número secreto.")
 
-print("Fim de jogo")
+print("Fim do jogo")
