c91df94301640878d00d8104c42e64ac01e94e44
Ana Paula <anapaulaoliveira.bio@outlook.com>
2023-02-08 16:25:51 -0300

    include chutes

--- forca.py
+++ forca.py
@@ -15,23 +15,42 @@ def jogar():
 
     while (not acertou and not enforcou):
 
-        chute = input("Qual letra? ")
-        chute = chute.strip()
+        chute = pede_chute()
 
-    if (chute in palavra_secreta):
-        index = 0
-        for letra in palavra_secreta:
-            if (chute.upper() == letra.upper()):
-                letras_acertadas[index] = letra
-            index = index + 1
+        if (chute in palavra_secreta):
+            marca_chute_correto(chute, letras_acertadas, palavra_secreta)
+        else:
+            erros += 1
+
+        enforcou = erros == 6
+        acertou = "_" not in letras_acertadas
+        print(letras_acertadas)
+
+    if (acertou):
+        imprime_mensagem_vencedor()
     else:
-        erros += 1
+        imprime_mensagem_perdedor()
 
-    enforcou = erros == 6
-    print(letras_acertadas)
+    print("Fim do jogo")
 
+def imprime_mensagem_vencedor():
+    print("Você ganhou!")
 
-print("Fim do jogo")
+def imprime_mensagem_perdedor():
+    print("Você perdeu!")
+
+def pede_chute():
+    chute = input("Qual letra? ")
+    chute = chute.strip().upper()
+
+    return chute
+
+def marca_chute_correto(chute, letras_acertadas, palavra_secreta):
+    index = 0
+    for letra in palavra_secreta:
+        if (chute == letra):
+            letras_acertadas[index] = letra
+        index += 1
 
 def imprime_mensagem_abertura():
     print("*********************************")
