59d16c3f0bdc649d5977e5cc83b950979eb606ac
Ana Paula <anapaulaoliveira.bio@outlook.com>
2023-02-08 15:55:50 -0300

    implement erro

--- forca.py
+++ forca.py
@@ -8,6 +8,7 @@ def jogar():
 
     enforcou = False
     acertou = False
+    erros = 0
 
     print(letras_acertadas)
 
@@ -16,17 +17,17 @@ def jogar():
         chute = input("Qual letra? ")
         chute = chute.strip()
 
+    if (chute in palavra_secreta):
         index = 0
         for letra in palavra_secreta:
             if (chute.upper() == letra.upper()):
                 letras_acertadas[index] = letra
             index = index + 1
+    else:
+        erros += 1
 
-        print(letras_acertadas)
-        print("Jogando...")
+    enforcou = erros == 6
+    print(letras_acertadas)
 
-        print("Fim do jogo")
 
-        if (__name__ == '__main__'):
-
-            jogar()
+print("Fim do jogo")
